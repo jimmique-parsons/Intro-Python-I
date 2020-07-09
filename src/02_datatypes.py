7eb361db2c2389bab996be130f4ee3435a6ba831
Jimmique <jimmique88@gmail.com>
2020-07-08 20:50:14 -0700

    Completed 00_hello to 03_modules

--- src/02_datatypes.py
+++ src/02_datatypes.py
@@ -14,8 +14,9 @@ y = "7"
 # Write a print statement that combines x + y into the integer value 12
 
 # YOUR CODE HERE
-
-
+y_int = int(y)
+print(x + y_int)
 # Write a print statement that combines x + y into the string value 57
 
 # YOUR CODE HERE
+print(str(5) + y)
